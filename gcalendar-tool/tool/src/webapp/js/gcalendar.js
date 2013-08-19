40db7a8daa4a06b167569e39d6f21add17812bb9
Richard Palacios <richpala@umich.edu>
2013-08-19 13:42:28 +0000

    University of Michigan UMICH-835 - Integrate Sakai + Google Calendar contrib tool into CTools Build - Use the new proxy server - crossdomain
    git-svn-id: https://source.sakaiproject.org/contrib/umich/google/gcalendar/trunk@84488 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- gcalendar-tool/tool/src/webapp/js/gcalendar.js
+++ gcalendar-tool/tool/src/webapp/js/gcalendar.js
@@ -7,7 +7,8 @@ var eventArray = []; // move globally
 var editable;
 
 var host = "../../"; // relative path to proxy
-var proxyName = "proxy-gcalendar";
+//var proxyName = "proxy-gcalendar";
+var proxyName = "crossdomain/gcalendar";
 
 var eventTimeValueArray = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", 
                            "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", 
